c65ecfa3be362b4a98f877076d4d0788286f52fa
gene <gene.drabkin@gmail.com>
2011-10-05 11:09:54 -0700

    added object.id to bast_in_place HTLM element ID to make sure IDs are unique on the page

--- lib/best_in_place/helper.rb
+++ lib/best_in_place/helper.rb
@@ -20,7 +20,7 @@ module BestInPlace
         collection = opts[:collection].to_json
       end
       out = "<span class='best_in_place'"
-      out << " id='best_in_place_#{object.class.to_s.gsub("::", "_").underscore}_#{field}'"
+      out << " id='best_in_place_#{object.class.to_s.gsub("::", "_").underscore}#{object.id}_#{field}'"
       out << " data-url='#{opts[:path].blank? ? url_for(object).to_s : url_for(opts[:path])}'"
       out << " data-object='#{object.class.to_s.gsub("::", "_").underscore}'"
       out << " data-collection='#{collection}'" unless collection.blank?
